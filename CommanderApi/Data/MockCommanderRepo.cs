2cb33051ee3f21ee269d422b5c42296c8076f516
Thiago <thagocruz@gmail.com>
2020-09-20 15:44:08 -0300

    Create MockCommanderRepo.cs

---
+++ CommanderApi/Data/MockCommanderRepo.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using CommanderApi.Models;
+
+namespace CommanderApi.Data
+{
+    public class MockCommanderRepo : ICommandRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            
+        }
+
+        public Command GetCommandById(int id)
+        {
+            
+        }
+    }
+}
