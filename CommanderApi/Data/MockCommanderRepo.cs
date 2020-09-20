6d453ad937f704e02c9c4ff40f85852ff59538c8
Thiago <thagocruz@gmail.com>
2020-09-20 15:44:45 -0300

    Implement GetAppCommands

--- CommanderApi/Data/MockCommanderRepo.cs
+++ CommanderApi/Data/MockCommanderRepo.cs
@@ -7,7 +7,14 @@ namespace CommanderApi.Data
     {
         public IEnumerable<Command> GetAppCommands()
         {
-            
+            var commands = new List<Command>
+            {
+                new Command {Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kattle & Pan"},
+                new Command {Id = 1, HowTo = "Cut bread", Line = "Get a knife", Platform = "Knife and chopping board"},
+                new Command {Id = 2, HowTo = "Make cup of tea", Line = "Place teabag in a cup", Platform = "Kattle & cup"}
+            };
+
+            return commands;
         }
 
         public Command GetCommandById(int id)
