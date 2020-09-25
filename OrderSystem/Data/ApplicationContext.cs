2e7b7ff80496d0d1fb9de9b4f26f2a3bced5b2a5
Thiago <thagocruz@gmail.com>
2020-09-25 00:08:57 -0300

    Overriding OnConfiguring

--- OrderSystem/Data/ApplicationContext.cs
+++ OrderSystem/Data/ApplicationContext.cs
@@ -4,6 +4,9 @@ namespace OrderSystem.Data
 {
     public class ApplicationContext : DbContext
     {
-        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\msqllocaldb;Initial Catalog=CursoEFCore;Integrated Security=true");
+        }
     }
 }
