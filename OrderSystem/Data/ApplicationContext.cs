026056f8d1f74dcc9191e97e46f4f4f4e7e7b382
Thiago <thagocruz@gmail.com>
2020-09-25 00:20:12 -0300

    Add client mapping

--- OrderSystem/Data/ApplicationContext.cs
+++ OrderSystem/Data/ApplicationContext.cs
@@ -1,12 +1,31 @@
 using Microsoft.EntityFrameworkCore;
+using OrderSystem.Domain;
 
 namespace OrderSystem.Data
 {
     public class ApplicationContext : DbContext
     {
+        public DbSet<Pedido> Pedidos { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=(localdb)\\msqllocaldb;Initial Catalog=CursoEFCore;Integrated Security=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>(p => 
+            {
+                p.ToTable("Clientes");
+                p.HasKey(p => p.Id);
+                p.Property(p => p.Nome).HasColumnType("VARCHAR(80)").IsRequired();
+                p.Property(p => p.Telefone).HasColumnType("CHAR(11)").IsRequired();
+                p.Property(p => p.CEP).HasColumnType("CHAR(8)").IsRequired();
+                p.Property(p => p.Estado).HasColumnType("CHAR(2)").IsRequired();
+                p.Property(p => p.Cidade).HasMaxLength(60).IsRequired();
+
+                p.HasIndex(i => i.Telefone).HasName("idx_cliente_telefone");
+            });
         }
     }
 }
