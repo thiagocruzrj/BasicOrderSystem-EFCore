dd18d2dd35bc1675c511618f7ec3f31c1733b5a5
Thiago <thagocruz@gmail.com>
2020-09-27 18:13:01 -0300

    Add log on dbcontext

--- OrderSystem/Data/ApplicationContext.cs
+++ OrderSystem/Data/ApplicationContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using OrderSystem.Data.Configuration;
 using OrderSystem.Domain;
 
@@ -6,13 +7,17 @@ namespace OrderSystem.Data
 {
     public class ApplicationContext : DbContext
     {
+        private static readonly ILoggerFactory _logger = LoggerFactory.Create(p => p.AddConsole());
         public DbSet<Pedido> Pedidos { get; set; }
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Cliente> Clientes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=CursoEFCore;Integrated Security=true"); //Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;
+            optionsBuilder
+                .UseLoggerFactory(_logger)
+                .EnableSensitiveDataLogging()
+                .UseSqlServer("Data Source=localhost\\SQLEXPRESS;Initial Catalog=CursoEFCore;Integrated Security=true"); //Server=localhost\SQLEXPRESS;Database=master;Trusted_Connection=True;
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
