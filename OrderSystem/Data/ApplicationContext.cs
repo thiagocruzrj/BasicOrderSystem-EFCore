fa063096af29234fd79ab6bc7079d7c38444d3a2
Thiago <thagocruz@gmail.com>
2020-09-27 18:00:07 -0300

    Add product and client properties on dbcontext

--- OrderSystem/Data/ApplicationContext.cs
+++ OrderSystem/Data/ApplicationContext.cs
@@ -7,6 +7,8 @@ namespace OrderSystem.Data
     public class ApplicationContext : DbContext
     {
         public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
